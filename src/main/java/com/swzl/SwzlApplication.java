4bf21c7a8a9c9d91270f136115dcd873d13de0e7
胡双 <hd1611756908@163.com>
2019-01-31 14:24:38 +0800

    创建微信小程序失物招领后台

---
+++ src/main/java/com/swzl/SwzlApplication.java
@@ -0,0 +1,14 @@
+package com.swzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwzlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwzlApplication.class, args);
+    }
+
+}
+
